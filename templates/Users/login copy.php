93600a1bc107eea5c1fc6a30c68af8081e9bcb2d
world3355 <u3355world@gmail.com>
2020-01-29 20:51:43 +0900

    user login css modify

---
+++ templates/Users/login copy.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @var \App\View\AppView $this
+ * @var \App\Model\Entity\User $user
+ */
+?>
+<head>
+    <style>
+        body {
+            margin-top:100px;
+            margin-left:100px;
+            background-color:rgb(249, 249, 249);
+            overflow:auto;
+            font-size:14px;
+            margin:0px;
+            line-height: 1;
+        }
+    </style>
+</head>
+<body>
+    <nav class="large-2 medium-4 columns" id="actions-sidebar">
+        <ul class="side-nav">
+            <li class="heading"><?= __('Actions') ?></li>
+            <li><?= $this->Html->link(__('유저 목록'), ['action' => 'index']) ?></li>
+        </ul>
+    </nav>
+    <div class="users form large-10 medium-8 columns content">
+    <div class="users form">
+        <?= $this->Flash->render() ?>
+        <?= $this->Form->create() ?>
+        <fieldset>
+        <legend><?= __('유저 로그인') ?></legend>
+        <?= $this->Form->control('email', ['label' => '이메일']) ?>
+        <?= $this->Form->control('password', ['label' => '비밀번호']) ?>
+        </fieldset>
+        <?= $this->Form->button(__('로그인')); ?>
+        <?= $this->Form->end() ?>
+    </div>
+    </div>
+</body>
--- templates/Users/login.php
+++ templates/Users/login.php
@@ -9,27 +9,119 @@
         body {
             margin-top:100px;
             margin-left:100px;
+            background-color:rgb(249, 249, 249);
+            overflow:auto;
+            font-size:14px;
+            margin:0px;
+            line-height: 1;
+            height: 100vh;
         }
+        
+        .login-page {
+            display : flex;
+            height: 100vh;
+            vertical-align: middle;
+            justify-content: center;
+            align-items: center;
+            padding: 100px 0px;
+            box-sizing : border-box;
+            letter-spacing: -0.5px;
+        }
+            
+        .login-form {
+            width:570px;
+            background: rgb(255, 255, 255) none repeat scroll 0% 0%;
+            padding : 60px 60px 50px 60px;
+            box-shadow : rgba(0, 0, 0, 0.1) 0px 3px 6px 0px;
+        }
+        
+        .main {
+            height: 100vh;
+        }
+        
+        .login-title-container {
+            display : flex;
+            justify-content: center;
+            margin-bottom : 45px;
+        }
+        
+        
+        .login-title {
+            font-family: 'Vibur', cursive;
+            font-size: 65px;
+        }
+        
+        .login-btn {
+            display : flex;
+            justify-content: center;
+            margin-top: 30px;
+        }
+        
+        
+        
+        
     </style>
 </head>
 <body>
-    <nav class="large-2 medium-4 columns" id="actions-sidebar">
-        <ul class="side-nav">
-            <li class="heading"><?= __('Actions') ?></li>
-            <li><?= $this->Html->link(__('유저 목록'), ['action' => 'index']) ?></li>
-        </ul>
-    </nav>
-    <div class="users form large-10 medium-8 columns content">
-    <div class="users form">
-        <?= $this->Flash->render() ?>
-        <?= $this->Form->create() ?>
-        <fieldset>
-        <legend><?= __('유저 로그인') ?></legend>
-        <?= $this->Form->control('email', ['label' => '이메일']) ?>
-        <?= $this->Form->control('password', ['label' => '비밀번호']) ?>
-        </fieldset>
-        <?= $this->Form->button(__('로그인')); ?>
-        <?= $this->Form->end() ?>
-    </div>
+    <div class="login-page">
+        <div class="login-back">
+            <div class="login-card">
+
+               <?= $this->Flash->render() ?>
+               <?= $this->Form->create() ?>
+
+                <div class="login-form">
+
+                   <div class="login-title-container">
+                       <div class="login-title">Login</div>
+                   </div>
+                    
+                    <hr>
+
+                    <?= $this->Form->control('email', ['label'=>'', 'class'=>'form-control','placeholder'=>'Enter id']) ?>
+                    <?= $this->Form->control('password', ['label'=>'', 'class'=>'form-control','placeholder'=>'Enter password']) ?>
+
+                   <div class="login-btn">
+                    <?= $this->Form->button('Login', ['class'=>'btn btn-primary']); ?>
+                    <?= $this->Form->end() ?>
+                   </div>
+                   
+                </div>
+
+
+            </div>
+        </div>
     </div>
 </body>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
